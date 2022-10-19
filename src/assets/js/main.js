9c5ea1d62ab6314a4f43b8317351c7496b7f0380
Jakub Samotyja <jjsamotyja@gmail.com>
2022-10-19 17:33:50 +0200

    First full working version

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,32 +1,51 @@
 import 'bootstrap';
-const inputPassword = document.querySelector("input");
+
+const inputPassword = document.querySelector('input');
 const inputLenght = document.querySelector('input.input-lenght');
-const btn = document.querySelector("button");
-const characters = [`!"#$%&'*+,./:;=?@\\^|~` + "`", '1234567890', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '_', '()[]{}<>'];
+const progressBar = document.querySelector('div.progress-bar');
+const progressText = document.querySelector('p.progress-bar-text');
+const liItems = [...document.getElementsByTagName('li')];
+const liIcons = [...document.querySelectorAll('svg.list-icon')];
+const btnGenerate = document.querySelector('button');
 const checkbox = [...document.querySelectorAll('input.option-checkbox')];
+const characters = ['abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ','1234567890',`!"#$%&'*+,./:;=?@\\^|~` + "`" , '_', '()[]{}<>'];
+
 let settings = [];
 let generatorChars = '';
 let generatorCharsLenght = '';
 let passwordStrength = 0;
 let result = '';
 
-const liItems = [...document.getElementsByTagName("li")]
-const progressBar = document.querySelector('div.progress-bar');
-console.log(progressBar)
-
-
-
-
-
 const generatePassword = function() {
+    
+    //Check if user provided length
+    let i = 0;
+    if (inputLenght.value == '') {
+        alert('You need to provide length of password');
+        return;
+    }
 
-    //Create array with selected settings
+    if (inputLenght.value <=0 || isNaN(inputLenght.value)) {
+        alert('You need to provide correct length of password');
+        return;
+    }
+
+    //Create array with selected settings and check if any checkbox is selected
     checkbox.forEach(checkbox => {
         settings.push(checkbox.checked);
+        if(checkbox.checked){
+            i += 1;
+        }
     });
 
+    //Display alert when none checbox is selected
+    if (i==0) {
+        alert('At least one option need to be checked');
+        return;
+    }
+
     //Create string with selected character sets
-    for (let i = 0; i < 6; i++) {
+    for (i = 0; i < 6; i++) {
         if (settings[i] === true) {
             generatorChars += characters[i];
         }
@@ -47,64 +66,88 @@ const generatePassword = function() {
     generatorChars = '';
 
     return;
-    
 };
+
 
 const checkPassword = function(){
     
     if (inputPassword.value.length >= 12) {
         passwordStrength += 1;
-        console.log("length ok");
-        liItems[0].style.color = "green";
+        liItems[0].style.color = "#008000";
+        liIcons[0].style.display = "block";
     }else{
-    }
+        liItems[0].style.color = "#ff0000";
+        liIcons[0].style.display = "none";
+    };
 
-    if (inputPassword.value.match(/[!"#$%&'*+,./:;=?@\^|~`]/)) {
+    if (inputPassword.value.match(/[!"#$%&'*+,./:;=?@\^|~`_<>{}()]/)) {
         passwordStrength += 1;
-        console.log("special ok");
-        liItems[1].style.color = "green";
+        liItems[1].style.color = "#008000";
+        liIcons[1].style.display = "block";
     }else{
-
-    }
+        liItems[1].style.color = "#ff0000";
+        liIcons[1].style.display = "none";
+    };
 
     if (inputPassword.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
         passwordStrength += 1;
-        console.log("upper adn lower ok");
-        liItems[2].style.color = "green";
+        liItems[2].style.color = "#008000";
+        liIcons[2].style.display = "block";
     }else{
-
-    }
+        liItems[2].style.color = "#ff0000";
+        liIcons[2].style.display = "none";
+    };
     
     if (inputPassword.value.match(/([0-9])/)) {
         passwordStrength += 1;
-        console.log("numbers ok");
-        liItems[3].style.color = "green";
+        liItems[3].style.color = "#008000";
+        liIcons[3].style.display = "block";
 
     }else{
+        liItems[3].style.color = "#ff0000";
+        liIcons[3].style.display = "none";
+    };
 
-    }
-    console.log(passwordStrength);
-    switch (passwordStrength) {
-        case 1:
-            progressBar.style.backgroundColor = "green";
-            break;
-        case 2:
-            
-            break;
-        case 3:
-            
-            break;
-        case 4:
-            
-            break;
-    
-        default:
-            break;
-    }
+    if (inputPassword.value !== '') {
+        switch (passwordStrength) {
+            case 2:
+                progressBar.style.backgroundColor = "#ff0000";
+                progressBar.style.width = "40%";
+                progressText.textContent = "Your password is weak";
+                progressText.style.color = "#ff0000";
+                break;
+            case 3:
+                progressBar.style.backgroundColor = "#008000";
+                progressBar.style.width = "60%";
+                progressText.textContent = "Your password is strong";
+                progressText.style.color = "#008000";
+                break;
+            case 4:
+                progressBar.style.backgroundColor = "#008000";
+                progressBar.style.width = "100%";
+                progressText.textContent = "Your password is very strong";
+                progressText.style.color = "#008000";
+                break;
+        
+            default:
+                progressBar.style.backgroundColor = "#ff0000";
+                progressBar.style.width = "20%";
+                progressText.textContent = "Your password is very weak";
+                progressText.style.color = "#ff0000";
+                break;
+        };
+
+    }else{
+                progressBar.style.backgroundColor = "#B2A8A8";
+                progressBar.style.width = "100%";
+                progressText.textContent = "Your password is empty";
+                progressText.style.color = "#B2A8A8";
+    };
+   
     passwordStrength = 0;
-}
+};
 
-btn.addEventListener("click", () => {
+btnGenerate.addEventListener("click", () => {
     generatePassword();
     checkPassword();
 })
